15a71b5947f334e1f9bdcf2e325218ae3c460617
benjaminkaplan <benjaminmichaelkaplan@gmail.com>
2018-03-26 12:36:31 -0400

    Add files via upload

--- DSA_ProblemSets/main.cpp
+++ DSA_ProblemSets/main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
-
+#include <fstream>
+#include <string.h>
+#include <sstream>
 using namespace std;
+
+template<typename T>
 class Node{
-  Template <class T>;
   T data;
   Node* next;
 };
 
+template<typename T>
 class SimpleList{
-    Node* first;
-    Node* last;
+    Node<T>* first;
+    Node<T>* last;
     string getName(){return this.name;}
   private:
       string name;
   protected:
 
 };
+
+
+void parseLine(string& str){
+    stringstream ss(str);
+    string temp;
+    while(ss>>str){
+      //ss>>str;
+      cout<<str<<endl;
+    }
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
+    ifstream input;
+    input.open("commands.txt");
+    string str;
+    while(getline(input,str) ){
+       //cout<<str<<endl;
+       parseLine(str);    
+    }
+    
     return 0;
-
 
 }
