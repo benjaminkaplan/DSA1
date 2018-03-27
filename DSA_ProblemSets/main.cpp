8da32c56a2081236c8516bf79c19dfc9cb1b2c14
benjaminkaplan <benjaminmichaelkaplan@gmail.com>
2018-03-27 00:11:23 -0400

    Add files via upload

--- DSA_ProblemSets/main.cpp
+++ DSA_ProblemSets/main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 template<typename T>
 class Node{
-  T data;
-  Node* next;
+  public:
+    T data;
+    Node* next;
+    Node(T t, Node* node){data = t; next = node;}
 };
 
 template<typename T>
@@ -22,7 +24,7 @@ class SimpleList{
 };
 
 
-void parseLine(string& str){
+void parseLine(string& str){ //NOT USED
     stringstream ss(str);
     string temp;
     while(ss>>str){
@@ -37,11 +39,24 @@ int main()
     ifstream input;
     input.open("commands.txt");
     string str;
-    while(getline(input,str) ){
+    string line;
+    while(getline(input,line) ){
        //cout<<str<<endl;
-       parseLine(str);    
+       stringstream ss(line);
+       ss>>str;
+       //cout<<"PROCESSING COMMAND: "<<line<<endl;
+       if(!(line.compare(0,6,"create"))) { //confirms presence of CREATE command
+         cout<<"create found"<<endl;
+         continue;
+       }
+       if(!(line.compare(0,4,"push"))){ //confirms presence of CREATE command
+         cout<<"push found"<<endl;
+       }
+       if(!(line.compare(0,3,"pop"))){ //confirms presence of CREATE command
+         cout<<"pop found"<<endl;
+       }
     }
-    
+
     return 0;
 
 }
