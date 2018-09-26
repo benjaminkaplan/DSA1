1ea079ef9b885ceecd062a26ddfb295f146f9a30
benjaminkaplan <benjaminmichaelkaplan@gmail.com>
2018-09-25 20:20:00 -0400

    Add files via upload

--- DSA2/hash.cpp
+++ DSA2/hash.cpp
@@ -1,20 +1,11 @@
 #include "hash.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 using namespace std;
-hashTable::hashTable(int size): capacity(getPrime(size)) , filled(0), data(getPrime(size)){
-  //capacity  = getPrime(size); // The current capacity of the hash table.
-  //filled = 0; // Number of occupied items in the table.
-  //vector<hashItem> data(getPrime(size));
-	/*
-	cout<<"prime: "<<getPrime(size)<<endl;
-	cout<<"capacity: "<<this->capacity<<endl;
-	cout<<"filledd: "<<this->filled<<endl;
-	*/
-}
+hashTable::hashTable(int size): capacity(getPrime(size)) , filled(0), data(getPrime(size)){}
 
 int hashTable::insert(const std::string &key, void *pv ){
-	//cout<<"filled"<<this->filled<<endl;
 	string hash_key = key;
 	int pos = hashTable::hash(hash_key);
 	hashItem item;
@@ -22,56 +13,43 @@ int hashTable::insert(const std::string &key, void *pv ){
 	item.isDeleted = false;
 	item.key = hash_key;
 	item.pv = pv;
-	if(!this->data[pos].isOccupied){ //entry is empty
-		//cout<<"entry was empty, inserting slot "<<pos<<endl;
-		//cout<<"sameness chaeck: "<<this->data[pos].key.compare(key)<<endl;
-		//if( !this->data[pos].key.compare(key) )
-		//	cout<<"same key found"<<endl;
-		//	return 1;
-		this->data[pos] = item;
-	}
-	
-	else if(this->data[pos].isOccupied){ //entry is taken
-		//std::vector<hashItem>::iterator it = this->data[pos];
-
-		while( this->data[pos].isOccupied){ //search for open slot
-			if(pos >= this->capacity)
-				pos = 0;	
-			//cout<<"entry is TAKEN, current occupant: "<<hash_key<<"\t slot: "<<pos<<endl;
-			
-			if( !this->data[pos].key.compare(key) )
-				return 1;
-			pos++;
+	while( this->data[pos].isOccupied){ //search for open slot				
+		if( !this->data[pos].key.compare(key) ){ //same key found
+			return 1;
 		}
-		this->data[pos] = item;
+		pos++;
+		if(pos >= this->capacity)
+			pos = 0;
+		
 	}
+	this->data[pos] = item;
 	this->filled++;
 	float filled_percentage = (float)this->filled/(float)this->capacity;	
-	//cout<<"filled percentage: "<<filled_percentage<<"\tfilled: "<<this->filled<<"\tcapacity: "<<this->capacity<<endl;
 	if(filled_percentage > 0.5){
-		//cout<<"rehashing"<<endl;	
-		if(!this->rehash()); // return true on failure
+		if(!this->rehash()); // return 2 on failure
 			return 2;
 	}
 	return 0;
 }
 
 bool hashTable::rehash(){
-	int size = getPrime(this->capacity + 1);
+	int size = getPrime(this->capacity);
 	vector<hashItem> old_table = this->data;
-	vector<hashItem> new_table(size);
+	vector<hashItem> new_table(1);
+	try{
+		new_table.resize(size);
+	}
+	catch(std::bad_alloc& ba ){
+		return false;
+	}
 	this->capacity = size;
 	this->filled = 0;
 	this->data = new_table;
 	for(int i = 0 ; i < old_table.size(); i++){
-		if(!old_table[i].isDeleted){ //not deleted
 			if(old_table[i].isOccupied){ //is occupied
-				//cout<<"inserting: "<<old_table[i].key<<endl;
 				this->insert(old_table[i].key,old_table[i].pv);
 			}
-		}
 	}
-
 	return true;
 }
 
@@ -86,7 +64,7 @@ int hashTable::findPos(const std::string &key){
 	int pos = hash(hash_key);
 	while(this->data[pos].isOccupied){
 		if(!this->data[pos].isDeleted){ //is NOT deleted
-			if(!this->data[pos].key.compare(hash_key)) //same key
+			if(!this->data[pos].key.compare(hash_key)) // is same key
 				return pos;
 		}
 		pos++;
@@ -94,8 +72,8 @@ int hashTable::findPos(const std::string &key){
 	return -1;
 }
 
-int hashTable::hash(const std::string &key){
-	const char* Key_Ptr = key.c_str();
+int hashTable::hash(const std::string &key){ //returns hash table position
+	const char* Key_Ptr = key.c_str();   //hash function taken from Weiss textbook pg. 184
 	unsigned int hash_val = 0;
 	while( *Key_Ptr )
 		hash_val = (hash_val << 5) + *Key_Ptr++;
@@ -105,11 +83,18 @@ int hashTable::hash(const std::string &key){
 
 
 unsigned int hashTable::getPrime(int size){
-	unsigned int primes[] = {53, 97 , 193 , 389 , 769 , 1543 , 3079 , 6151 , 12289 , 24593 , 49157 , 98317 , 196613 , 393241 , 786433 , 1572869 , 3145739 , 6291469 , 12582917 , 25165843 ,50331653 , 100663319 , 201326611 , 402653189 , 805306457 , 1610612741}; // list of primes taken from 
-																								 //https://planetmath.org/goodhashtableprimes
+	unsigned int primes[] = {53, 97 , 193 , 389 , 769 , 1543 , 
+				3079 , 6151 , 12289 , 24593 , 49157 , 
+				98317 , 196613 , 393241 , 786433 , 
+				1572869 , 3145739 , 6291469 , 
+				12582917 , 25165843 ,50331653 , 
+				100663319 , 201326611 , 402653189 , 
+				805306457 , 1610612741}; // list of primes taken from
+							 //https://planetmath.org/goodhashtableprimes
 	for( int i = 0; i < 26; i++){
-		if( primes[i] > size)
+		if( primes[i] > 2*size)
 			return primes[i];
 	}
-	return 0; // run out of sizes
+	return 1610612741; // run out of sizes
 }
+
--- DSA2/spellcheck.cpp
+++ DSA2/spellcheck.cpp
@@ -3,21 +3,24 @@
 #include "hash.h"
 #include <ctime>
 using namespace std;
+
+bool contains_number(const std::string &str)
+{
+    return (str.find_first_of("0123456789") != std::string::npos);
+}   
+
 int main(){
 	
 	
 	cout <<"Enter name of dictionary: ";
 	string dict_name = "";
-	//cin >> dict_name;
-	dict_name = "wordlist_small.txt";
+	cin >> dict_name;
 	cout << "Enter name of input file: ";
 	string input_name = "";
-	//cin >> input_name;
-	input_name = "lyrics.txt";
+	cin >> input_name;
 	cout << "Enter name of output file: ";
 	string output_name = "";
-	//cin >> output_name;
-	output_name = "outfile.txt";
+	cin >> output_name;
 	
 	ifstream input_file;
 	input_file.open(input_name);
@@ -25,32 +28,60 @@ int main(){
 	dict_file.open(dict_name);
 	ofstream output_file;
 	output_file.open(output_name);
-	hashTable table = hashTable(100) ;	
+
+	hashTable table = hashTable(1000) ;	
 	string line = "a";
 	int start_s=clock();
-	while( line.length() ){
-		getline(dict_file,line);
-		table.insert(line);
+	char letter = 'a';
+	string word = "";
+	while( (letter = dict_file.get()) != EOF ){
+		if(letter == '\n'){
+			table.insert(word);
+			word = "";
+		}else{
+			if(letter != '\r'){ //protects against pesky carriage returns
+				word += tolower(letter);
+			}
+		}
 	}
 	int stop_s=clock();
-	cout << "Total time (in seconds) to check document: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << endl;
+	dict_file.close();
+	cout << "Total time (in seconds) to load dictionary: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << endl;
 
 	int line_counter = 1;
 	int word_length = 0;
-	string word = "";
-	char letter = 'a';
-	while( (letter = input_file.get() ) != EOF){
-		while( (letter >= 48 && letter <= 57 )|| 
-		        letter =='-' || 
-                        letter== '\''  || 
-                       (letter >= 65 && letter <= 122) ){
-			word += letter;
+	bool long_flag = false;
+	start_s = clock();
+	while( (letter = input_file.get() ) != EOF){ //word separators 0-9, -, ', A-Z, a-z
+		if( (letter >= '0' && letter <= '9' )|| 
+		     letter =='-' || 
+                     letter== '\''  || 
+                    (letter >= 'A' && letter <= 'Z') ||
+    		    (letter >= 'a' && letter <= 'z') ){
+			if(!long_flag){
+				word += tolower(letter);
+				word_length++;
+			}
+		
+		} else{
+			if(word.length() && !table.contains(word) && !long_flag && !contains_number(word)){
+				output_file<<"Unknown word at line "<<line_counter<<": "<<word<<endl;
+			}
+			word = "";
+			word_length = 0;
+			long_flag = false;
+		}	
+		if(letter == '\n')
+			line_counter++;
+		if(word_length > 20 && !long_flag){
+			output_file<< "Long word at line "<<line_counter<<", starts: " << word.substr(0,20)<<endl;
+			long_flag= true;
 		}
-		cout<<word<<endl;
-		word = "";
 	}
-	//cout<<"pos of zygote: "<<table.contains("Zionism")<<endl;
-	dict_file.close();
+	stop_s=clock();
+	cout << "Total time (in seconds) to check document: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << endl;
 	input_file.close();
+	output_file.close();
 	return 0;
 }
+
